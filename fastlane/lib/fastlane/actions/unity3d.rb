3528a4c616dbac18861a0030b38fd2cbf4e5f6bc
오준환 <junwhan.oh@nhnent.com>
2016-07-28 10:35:16 +0900

    Add fastlane unity3d action

--- fastlane/lib/fastlane/actions/unity3d.rb
+++ fastlane/lib/fastlane/actions/unity3d.rb
@@ -1,39 +1,42 @@
 module Fastlane
   module Actions
-    class Unity3dAction < Action
-      def self.run(values)
-        require 'unity3d'
+    module SharedValues
+      UNITY3D_CUSTOM_VALUE = :UNITY3D_CUSTOM_VALUE
+    end
 
-        begin
+    class Unity3dAction < Action
+      def self.run(params)
+        require 'unity3d'
+        
           FastlaneCore::UpdateChecker.start_looking_for_update('unity3d') unless Helper.is_test?
 
-          Unity3d::Manager.new.work(values)
-
-          true
-        ensure
-          FastlaneCore::UpdateChecker.show_update_status('unity3d', Unity3d::VERSION)
-        end
+          Unity3d::Manager.new.work(params)
       end
 
       def self.description
-        "Easily export your unity3d app using `unity3d`"
+        "Easily build and export your Unity3d app "
       end
 
       def self.details
-        "More information: https://github.com/fastlane/fastlane/tree/master/scan"
-      end
-
-      def self.author
-        "Junhwan Oh"
+        "More information: https://github.com/smallmiro/fastlane/tree/master/unity3d"
       end
 
       def self.available_options
         require 'unity3d'
-        Unity3d::Options.available_options
+      	Unity3d::Options.available_options
+      end
+
+      def self.return_value
+        "The absolute path to the generated apk file or iOS project"
+      end
+
+      def self.authors
+        # So no one will ever forget your contribution to fastlane :) You are awesome btw!
+        "Junhwan Oh"
       end
 
       def self.is_supported?(platform)
-        [:ios, :android].include? platform
+        true
       end
     end
   end
